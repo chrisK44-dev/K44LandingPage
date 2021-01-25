7a96cf9e1c77379fc4f9973cab11ef1d085a061b
murrmna95 <christopher.murray10@gmail.com>
2021-01-25 21:46:03 +0100

    Initial push for Website

---
+++ src/components/about.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+export class about extends Component {
+  render() {
+    return (
+        <div id="about">
+        <div className="container">
+          <div className="row">
+            <div className="col-xs-12 col-md-6"> <img src="img/green_retail.jpg" className="img-responsive" alt=""/> </div>
+            <div className="col-xs-12 col-md-6">
+              <div className="about-text">
+                <h2>About Us</h2>
+                <p>{this.props.data ? this.props.data.paragraph : 'loading...'}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default about
