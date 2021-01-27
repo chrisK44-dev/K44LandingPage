4e73b868d47233907db838770ca80c0ec131a308
murrmna95 <christopher.murray10@gmail.com>
2021-01-27 13:48:54 +0100

    Changed about us copy, pic is b/w now

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -10,67 +10,18 @@ export class Contact extends Component {
               <div className="row">
                 <div className="section-title">
                   <h2>Get In Touch</h2>
-                  <p>
-                    Please fill out the form below to send us an email and we
-                    will get back to you as soon as possible.
-                  </p>
                 </div>
-                <form name="sentMessage" id="contactForm" noValidate>
-                  <div className="row">
-                    <div className="col-md-6">
-                      <div className="form-group">
-                        <input
-                          type="text"
-                          id="name"
-                          className="form-control"
-                          placeholder="Name"
-                          required="required"
-                        />
-                        <p className="help-block text-danger"></p>
-                      </div>
-                    </div>
-                    <div className="col-md-6">
-                      <div className="form-group">
-                        <input
-                          type="email"
-                          id="email"
-                          className="form-control"
-                          placeholder="Email"
-                          required="required"
-                        />
-                        <p className="help-block text-danger"></p>
-                      </div>
-                    </div>
+                  <div className="contact-item">
+                    <h3>Contact Info</h3>
                   </div>
-                  <div className="form-group">
-                    <textarea
-                      name="message"
-                      id="message"
-                      className="form-control"
-                      rows="4"
-                      placeholder="Message"
-                      required
-                    ></textarea>
-                    <p className="help-block text-danger"></p>
+                  <div className="contact-item">
+                    <p>
+                      <span>
+                        <i className="fa fa-envelope-o"></i> Email
+                      </span>{" "}
+                      {this.props.data ? this.props.data.email : "loading"}
+                    </p>
                   </div>
-                  <div id="success"></div>
-                  <button type="submit" className="btn btn-custom btn-lg">
-                    Send Message
-                  </button>
-                </form>
-              </div>
-            </div>
-            <div className="col-md-3 col-md-offset-1 contact-info">
-              <div className="contact-item">
-                <h3>Contact Info</h3>
-              </div>
-              <div className="contact-item">
-                <p>
-                  <span>
-                    <i className="fa fa-envelope-o"></i> Email
-                  </span>{" "}
-                  {this.props.data ? this.props.data.email : "loading"}
-                </p>
               </div>
             </div>
           </div>
