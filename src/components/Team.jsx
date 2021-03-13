7013fb8d9223425f2d7ba78a8447702792dc0e75
murrmna95 <christopher.murray10@gmail.com>
2021-03-13 20:24:45 +0100

    Fixed reactive-ness

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -11,7 +11,7 @@ export class Team extends Component {
           <div id="row">
             {this.props.data
               ? this.props.data.map((d, i) => (
-                  <div  key={`${d.name}-${i}`} className="col-md-4 col-sm-8 team">
+                  <div  key={`${d.name}-${i}`} className="col-md-4 col-sm-12 team">
                     <div className="thumbnail">
                       <img src={d.img} alt="..." className="team-img" />
                       <div className="caption">
